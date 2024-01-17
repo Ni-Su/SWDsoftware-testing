db1c5b62f0ed72bf4a1bf81a3a0452b91cfca5ff
Ni-Su <nikolaysukhikh81@gmail.com>
2024-01-17 22:05:21 +0500

    Исправил импорт OpenPageTests

--- src/test/java/ru/stqa/swd/litecart/tests/OpenPageTests.java
+++ src/test/java/ru/stqa/swd/litecart/tests/OpenPageTests.java
@@ -3,7 +3,6 @@ package ru.stqa.swd.litecart.tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
